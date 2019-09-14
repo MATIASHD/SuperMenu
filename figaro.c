4d2ba50ed71c86d13d54bf8369140e11b0b4bc39
Matias Arevalo <arevalo.matias418@gmail.com>
2019-09-14 10:52:03 -0300

    Se agregaron dos figaros

--- figaro.c
+++ figaro.c
@@ -6,6 +6,8 @@
 
 sem_t semFiiiiigaro;
 sem_t semFigaro;
+sem_t semFigaro1;
+sem_t semFigaro2;
 sem_t semFigaroFi;
 sem_t semFigaroFa;
 
@@ -25,11 +27,33 @@ void *imprimirFigaro () {
     for (int i = 0; i < cantidad; i++)
     {
         sem_wait(&semFigaro);
-        printf("Figaro");
+        printf("Figaro ");
+        sem_post(&semFigaro1);
+    }
+    pthread_exit(NULL);
+}
+
+/////////////////////////////
+void *imprimirFigaro1 () {
+    for (int i = 0; i < cantidad; i++)
+    {
+        sem_wait(&semFigaro1);
+        printf("Figaro ");
+        sem_post(&semFigaro2);
+    }
+    pthread_exit(NULL);
+}
+
+void *imprimirFigaro2 () {
+    for (int i = 0; i < cantidad; i++)
+    {
+        sem_wait(&semFigaro2);
+        printf("Figaro \n");
         sem_post(&semFigaroFi);
     }
     pthread_exit(NULL);
 }
+/////////////////////////////
 
 void *imprimirFigaroFi() {
     for (int i = 0; i < cantidad; i++)
@@ -55,20 +79,26 @@ void *imprimirFigaroFa(){
 void *Sem(){
     sem_init(&semFiiiiigaro,0,1);
     sem_init(&semFigaro,0,0);
+    sem_init(&semFigaro1,0,0);
+    sem_init(&semFigaro2,0,0);
     sem_init(&semFigaroFi,0,0);
     sem_init(&semFigaroFa,0,0);
 }
 
 void *hilos(){
-    pthread_t hiloFiiiiigaro, hiloFigaro, hiloFigaroFi, hiloFigaroFa;
+    pthread_t hiloFiiiiigaro, hiloFigaro, hiloFigaro1, hiloFigaro2, hiloFigaroFi, hiloFigaroFa;
 
     pthread_create(&hiloFiiiiigaro, NULL, imprimirFiiiiigaro, NULL);
     pthread_create(&hiloFigaro, NULL, imprimirFigaro, NULL);
+    pthread_create(&hiloFigaro1, NULL, imprimirFigaro1, NULL);
+    pthread_create(&hiloFigaro2, NULL, imprimirFigaro2, NULL);
     pthread_create(&hiloFigaroFi, NULL, imprimirFigaroFi, NULL);
     pthread_create(&hiloFigaroFa, NULL, imprimirFigaroFa, NULL);
 
     pthread_join(hiloFiiiiigaro, NULL);
     pthread_join(hiloFigaro, NULL);
+    pthread_join(hiloFigaro1, NULL);
+    pthread_join(hiloFigaro2, NULL);
     pthread_join(hiloFigaroFi, NULL);
     pthread_join(hiloFigaroFa, NULL);
 
@@ -77,6 +107,8 @@ void *hilos(){
 void *destruirSem() {
     sem_destroy(&semFiiiiigaro);
     sem_destroy(&semFigaro);
+    sem_destroy(&semFigaro1);
+    sem_destroy(&semFigaro2);
     sem_destroy(&semFigaroFi);
     sem_destroy(&semFigaroFa);
 }
@@ -85,7 +117,7 @@ int main () {
     printf("Ingrese cantidad de figaros a entonar: ");
     scanf("%i", &cantidad);
 
-    sem();
+    Sem();
     hilos();
 
     pthread_exit(NULL);
