df794f68981a85c5ebe53cbc69c10461d19518c6
nico-4164 <44095932+nico-4164@users.noreply.github.com>
2019-09-29 16:23:59 -0300

    mi version del barbero

--- Barbero_v2.c
+++ Barbero_v2.c
@@ -6,112 +6,132 @@
 #include <time.h>
 
 int cantClientes = 0;
-int cliente = 1;
 int sillasTotales = 3;
 int duermeBarbero; //0 esta durmiendo y 1 esta despierto
 //int ubicacionDisponible = 3; //Cantidad de sillas contando la del barbero
 
 sem_t semBarbero, semCliente;
 
-pthread_mutex_t mutex;
+pthread_mutex_t atendiendo;
+pthread_mutex_t ingresando;
 
 /*------------------ ESTA SECCION PERTENECE AL BARBERO -------------------*/
 
 void *BarberoDormilon(){
     
     while(1){
-        sleep(1);
-        sem_wait(&semBarbero);
 
-        pthread_mutex_lock(&mutex);
+        sem_wait(&semBarbero);
+        pthread_mutex_lock(&atendiendo);
+        fflush(stdout);
 
         if(cantClientes == 0){
             duermeBarbero = 0; //Usar true o false para el estado del barbero
             printf("-BARBERO- ZZZZZZZ \n");
+
 	    
         }
 
-	else{
-		
-		printf("-BARBERO- XXXXX \n");          	
-		cantClientes--;
-		
-		printf("-BARBERO- Cliente en espera: %d \n Ubicaciones: %d \n", cantClientes, sillasTotales-cantClientes);
-		
-		//sem_post(&semCliente);
-	}
+	   else{
 
-        pthread_mutex_unlock(&mutex);
-        sem_post(&semCliente);
+		cantClientes--;
+		duermeBarbero = 1;
+
+		printf("-BARBERO- XXXXX \n"); 
+		printf("-BARBERO- Cliente en espera: %d \n Ubicaciones: %d \n", cantClientes, sillasTotales-cantClientes);
+        printf("\n");
+		
+	   }
+
+       pthread_mutex_unlock(&atendiendo);
     }
 }
 
 /*------------------ ESTA SECCION PERTENECE AL CLIENTE -------------------*/
 
-void *Clientes(){
+void *Cliente(){
 
     while (1){
-        sem_wait(&semCliente);
-        sleep(1);
 
-	srand(time(NULL));
-	cliente = rand() % 3;
-        pthread_mutex_lock(&mutex);
-	cantClientes+=0;
-	if (cantClientes < sillasTotales-cantClientes){
-	      //cantClientes ++;
-              printf("-CLIENTE- Cliente en espera: %d \n Ubicaciones: %d \n", cantClientes, sillasTotales-cantClientes);
-              printf("PRUEBA%d",cantClientes);
-              /*----- EL CLIENTA ESTA EN ESPERA -----*/
-              if (duermeBarbero == 1){
-                 printf("-CLIENTE- Leyendo revista...\n");
-              }
+        //inicializar la semilla del generador random:
+        int microseconds; srand (time(NULL));
 
-               /*----- EL PRIMER CLIENTE DESPIERTA AL BARBERO -----*/
-               if (duermeBarbero == 0 && cantClientes>0) {
-		  
-                  printf("ejem, ejem..., cof, cof... \n");
-                  duermeBarbero = 1;
-		  
-            } 
+        //generar un numer random entre 1 y 5:
+        microseconds = rand() % 5 + 1;
+
+        //dormir el thread, simula que esta haciendo alguna tarea
+        sleep(microseconds);
+
+        //muestra los print de la funcion a medida que se ejecuta la funcion, 
+        //se necesita usar para cuando se usan sleeps, de lo contrario los prints se muestran cuando termina la ejecucion de la funcion
+        //y una funcion puede empezar antes y terminar su ejecucion despues de otro hilo y confundirse los mensajes
+        fflush(stdout);
+        pthread_mutex_lock(&ingresando);
+
+	if (cantClientes < sillasTotales){
+         
+        /*----- EL CLIENTA ESTA EN ESPERA -----*/
+		if (duermeBarbero == 1){
+            printf("-Llego cliente- Leyendo revista...\n");
+            printf("Cliente en espera: %d \n Ubicaciones: %d \n", cantClientes, sillasTotales-cantClientes-1);
+        }
+
+            /*----- EL PRIMER CLIENTE DESPIERTA AL BARBERO -----*/
+		else if (duermeBarbero == 0){
+
+            fflush(stdout);
+            duermeBarbero = 1;
+            printf("-Llego cliente- ejem, ejem..., cof, cof... \n");
+        }
+ 
+        cantClientes ++; 	
 
         }   
     
         /*----- EL CLIENTE SE VA POR QUE ESTA LLENO EL LUGAR -----*/
-        else {
-            printf("-CLIENTE- Chau $ chau $ \n");
+    else {
+            printf("-Llego cliente- Chau $ chau $ \n");
         }
 
-	pthread_mutex_unlock(&mutex);
+    printf("\n");
 
-        sem_post(&semBarbero);
+    sem_post(&semBarbero);
+	pthread_mutex_unlock(&ingresando);
+    pthread_exit(NULL);
+
     }
 }
 
 void *main(){
+
     printf("Comienza: \n");
     printf("\n");
 
     sem_init(&semBarbero,0,1); // 1 es el barbero durmiendo 
     sem_init(&semCliente,0,0);
 
-    pthread_mutex_init(&mutex, NULL);
-    pthread_t t1,t2,t3,t4,t5,t6,t7;
+    pthread_mutex_init(&atendiendo, NULL);
+    pthread_mutex_init(&ingresando, NULL);
 
-    pthread_create(&t1, NULL, BarberoDormilon, NULL);
-    pthread_create(&t2, NULL, Clientes, NULL);
-    pthread_create(&t3, NULL, Clientes, NULL);
-    pthread_create(&t4, NULL, Clientes, NULL);
-    pthread_create(&t5, NULL, Clientes, NULL);
-    pthread_create(&t6, NULL, Clientes, NULL);
-    pthread_create(&t7, NULL, Clientes, NULL);
+    pthread_t hilo1,hilo2,hilo3,hilo4,hilo5,hilo6,hilo7;
+
+    pthread_create(&hilo1, NULL, BarberoDormilon, NULL);
+
+    pthread_create(&hilo2, NULL, Cliente, NULL);
+    pthread_create(&hilo3, NULL, Cliente, NULL);
+    pthread_create(&hilo4, NULL, Cliente, NULL);
+    pthread_create(&hilo5, NULL, Cliente, NULL);
+    pthread_create(&hilo6, NULL, Cliente, NULL);
+    pthread_create(&hilo7, NULL, Cliente, NULL);
 
 
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    pthread_join(t3, NULL);
-    pthread_join(t4, NULL);
-    pthread_join(t5, NULL);
-    pthread_join(t6, NULL);
-    pthread_join(t7, NULL);
+    pthread_join(hilo1, NULL);
+    pthread_join(hilo2, NULL);
+    pthread_join(hilo3, NULL);
+    pthread_join(hilo4, NULL);
+    pthread_join(hilo5, NULL);
+    pthread_join(hilo6, NULL);
+    pthread_join(hilo7, NULL);
+
+
 }
